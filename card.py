ec9c61a0622376cc67c35f801b2985e87e127be9
PatrickFS18 <84478387+PatrickFS18@users.noreply.github.com>
2024-11-19 15:52:16 -0300

    Update card.py

--- card.py
+++ card.py
@@ -1,9 +1,9 @@
 import random 
 
 class Carta:
-    def __init__(self, valor, naipe):
-        self.valor = valor
-        self.naipe = naipe
+    def __init__(self,dictCard):
+        self.valor = dictCard['value']
+        self.naipe = dictCard['naipe']
 
     def __str__(self):
         return f"{self.valor} de {self.naipe}"
@@ -14,15 +14,14 @@ class Baralho:
 
     def gerarCartas(self):
         for i in range(1, 14):  # 1 a 13 (de Ás a Rei)
-            self.cartas.append(Carta(i, 'Paus'))
-            self.cartas.append(Carta(i, 'Ouro'))
-            self.cartas.append(Carta(i, 'Espada'))
-            self.cartas.append(Carta(i, 'Copas'))
+            self.cartas.append(Carta({'value':i, 'naipe': 'Paus'}))
+            self.cartas.append(Carta({'value':i, 'naipe': 'Ouro'}))
+            self.cartas.append(Carta({'value':i, 'naipe':'Espada'}))
+            self.cartas.append(Carta({'value':i, 'naipe':'Copas'}))
             
     
     def embaralharCartas(self):
         random.shuffle(self.cartas) 
-    
 
 class Jogador:
     def __init__(self):
@@ -36,18 +35,26 @@ baralho.gerarCartas()
 baralho.embaralharCartas()
 
 cartas = baralho.cartas
+for i in cartas:
+    print(i)
 
 jogador = Jogador()
 adversario = Jogador()
-
+dealer = Jogador()
 for i in range (0,4,2):
     jogador.mao.append(cartas[i])
     cartas.pop(i)
+    
 for i in range(0,2):
     adversario.mao.append(cartas[i])
     cartas.pop(i)
-for i in jogador.mao:
-    print(i)
-for i in adversario.mao:
-    print(i)
-
+    
+ for i in jogador.mao:
+     print(i)
+    
+ for i in adversario.mao:
+     print(i)
+ for i in range (0,5):
+     dealer.mao.append(cartas[i])
+ for i in dealer.mao:
+     print(i)
