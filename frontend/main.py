925385d4a5f8d4dc7641fa8aacef75b51faccff8
PatrickFS18 <patrick.fs.dev@gmail.com>
2024-12-27 19:33:18 -0300

    ajustes

--- frontend/main.py
+++ frontend/main.py
@@ -10,55 +10,11 @@ from utils.servidorSocket import ServidorSocket
 class Poker:
     def __init__(self):
         self.cliente_socket = ServidorSocket()
-        
-        self.jogador = Jogador()
-        self.adversario = Jogador()
-        self.dealer = Jogador()
-        self.baralho = Baralho()
-        self.is_initialized = False
-        self.verify = True
-        self.compare_1_1 = None
-        self.compare_1_2 = None        
-        self.compare = None
         self.sala_selecionada_index = 0 
         self.salas_list = None
         pyxel.init(160,120)
         pyxel.run(self.update,self.draw)
         
-        
-    def initializedGame(self):
-        if(self.is_initialized == False):
-            # Instância de Initialização dos jogadores e Mesa e embaralhamento de cartas
-            
-            # self.baralho.gerarCartas()
-            # self.baralho.embaralharCartas()
-            # cartas = self.baralho.cartas
-            
-            # # Distribuição de cartas 
-            # for i in range (0,4,2):
-            #     self.jogador.mao.append(cartas[i])
-            #     cartas.pop(0)
-                
-            # for i in range(0,2):
-            #     self.adversario.mao.append(cartas[i])
-            #     cartas.pop(0)
-                
-            # for i in range (0,5):
-            #     self.dealer.mao.append(cartas[i])
-            #     cartas.pop(0)
-            
-            # CHAMAR FUNÇÃO DO BACKEND PARA PEGAR ESSA INFORMAÇÃO
-            self.is_initialized = True
-    
-    def orderByValueAndNaipe(self,cards):
-        
-        # Ordem dos naipes (Paus, Ouro, Espada, Copas)
-        order_naipes = {'Paus': 0, 'Ouro': 1, 'Espada': 2, 'Copas': 3}
-        
-        # Ordena a mão,  primeiro pelo naipe e depois pelo valor
-
-        cards = cards.sort(key = lambda carta: (order_naipes[carta.naipe], carta.valor))
-        return cards
 
     def update(self):
         self.salas_list = self.cliente_socket.salas_disponiveis
